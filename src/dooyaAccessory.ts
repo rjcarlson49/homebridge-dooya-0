535f70768b3e0ef90a90f9f7f812e76362559428
Bob Carlson <bob@theralynx.com>
2020-06-09 16:56:54 -0700

    First commit

--- src/dooyaAccessory.ts
+++ src/dooyaAccessory.ts
@@ -1,38 +1,39 @@
 import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { DooyaHomebridgePlatform } from './platform';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class DooyaAccessory {
   private service: Service;
 
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private shadeStates = {
+    currentPosition: 100, // 0-100, 100 is fully open, 0 is fully closed
+    targetPosition: 100,
+    positionState: 2,     // 0 - decreasing, 1 - increasing, 2 - stopped
   }
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: DooyaHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Dooya')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Shades')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, '0000-0000');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // get the WindowCovering service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.WindowCovering) || this.accessory.addService(this.platform.Service.WindowCovering);
 
     // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
     // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
@@ -45,21 +46,26 @@ export class ExamplePlatformAccessory {
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    // register handlers for the CurrentPosition Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
+      .on('get', this.getCurrentPos.bind(this)); // GET - bind to the `getCurrentPos` method below
+    
+    // register handlers for the TargetPosition Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)!
+      .on('get', this.getTargetPos.bind(this))        // GET - bind to the 'getTarget` method below
+      .on('set', this.setTargetPos.bind(this));       // SET - bind to the 'setTarget` method below
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
+    // register handlers for the PositionState Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.PositionState)
+      .on('get', this.getPosState.bind(this));       // GET - bind to the 'getPosState` method below
+    
     // EXAMPLE ONLY
     // Example showing how to update the state of a Characteristic asynchronously instead
     // of using the `on('get')` handlers.
     //
     // Here we change update the brightness to a random value every 5 seconds using 
     // the `updateCharacteristic` method.
+    /*
     setInterval(() => {
       // assign the current brightness a random value between 0 and 100
       const currentBrightness = Math.floor(Math.random() * 100);
@@ -69,21 +75,7 @@ export class ExamplePlatformAccessory {
 
       this.platform.log.debug('Pushed updated current Brightness state to HomeKit:', currentBrightness);
     }, 10000);
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
-
-    // you must call the callback function
-    callback(null);
+    */
   }
 
   /**
@@ -99,32 +91,57 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-  getOn(callback: CharacteristicGetCallback) {
+  getCurrentPos(callback: CharacteristicGetCallback) {
 
     // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    const currentPosition = this.shadeStates.currentPosition;
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    this.platform.log.debug('Get Characteristic CurrentPos ->', currentPosition);
 
     // you must call the callback function
     // the first argument should be null if there were no errors
     // the second argument should be the value to return
-    callback(null, isOn);
+    callback(null, currentPosition);
+  }
+
+  getTargetPos(callback: CharacteristicGetCallback) {
+
+    // implement your own code to check if the device is on
+    const targetPosition = this.shadeStates.targetPosition;
+
+    this.platform.log.debug('Get Characteristic TargetPos ->', targetPosition);
+
+    // you must call the callback function
+    // the first argument should be null if there were no errors
+    // the second argument should be the value to return
+    callback(null, targetPosition);
+  }
+
+  getPosState(callback: CharacteristicGetCallback) {
+
+    // implement your own code to check if the device is on
+    const posState = this.shadeStates.positionState;
+
+    this.platform.log.debug('Get Characteristic PositionState ->', posState);
+
+    // you must call the callback function
+    // the first argument should be null if there were no errors
+    // the second argument should be the value to return
+    callback(null, posState);
   }
 
   /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, changing the Brightness
    */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  setTargetPos(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
     // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+    this.shadeStates.targetPosition = value as number;
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    this.platform.log.debug('Set Characteristic Target Position -> ', value);
 
     // you must call the callback function
     callback(null);
   }
-
 }
